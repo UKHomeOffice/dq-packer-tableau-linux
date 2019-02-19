018a3d2d7bf4d166593295b0af97b0a6a709472c
Ian Relf <ian.relf@gmail.com>
2019-02-19 12:36:35 +0000

    Will read and publish ALL DataSources in a repo (hopefully)

--- tableau-scripts/tableau-pub.py
+++ tableau-scripts/tableau-pub.py
@@ -7,7 +7,7 @@
 
 # All artefacts are found in Git
 # Assume Git structure:
-#REPO
+# REPO
 # |-- README.md
 # |-- datasources
 # |   |-- Project1
@@ -31,40 +31,73 @@ from os import listdir
 import subprocess
 
 
-# Global vars
-if len(sys.argv) == 2:
-    site = sys.argv[1]
-else:
-    print "Usage: " + sys.argv[0] + " [SITE]"
-    sys.exit(1)
-home_dir = os.environ['HOME']
-
-
-
 # Define a function to read ALL artefacts from Git REPO
+# Assume that all artefacts are for the SITE passed in to this python script
+# Future enhancement: Site will be the highest level directory within repo
 def read_artefacts_all(repo_name):
     # Get all DataSource Projects
     ds_list = []
 
-    print ("repo name = <" + repo_name + ">")
-    for proj_ds in listdir(os.path.join(home_dir, repo_name, 'datasources')):
-        print ("proj = <" + proj_ds + ">")
-        for ds in listdir(os.path.join(home_dir, repo_name, 'datasources', proj_ds)):
-            print ("ds = <" + ds + ">")
+    if verbose >= 1:
+        print ("repo name = <" + repo_name + ">")
+    for proj_ds in listdir(os.path.join(repo_name, 'datasources')):
+        if verbose >=1:
+            print ("proj = <" + proj_ds + ">")
+        for ds in listdir(os.path.join(repo_name, 'datasources', proj_ds)):
+            if verbose >= 3:
+                print ("ds = <" + ds + ">")
             full_ds = []
             full_ds.append(site)
             full_ds.append(proj_ds)
             full_ds.append(ds)
             ds_list.append(full_ds)
-
+    if verbose >= 2:
         print ds_list
+    return ds_list
 
 
 # Define a function to read Specific artefacts from environment variable
 def read_artefacts_specific():
     pass
 
-# Start here
+# Define a function to publish all of the artefacts read previously
+def publish_datasources(repo_name, ds_list_to_pub):
+    for ds_to_pub in ds_list_to_pub:
+        if verbose >= 2:
+            print "ds_to_pub = <"
+            for elem in ds_to_pub:
+                print elem
+            print ">"
+        project = ds_to_pub[1]
+        ds_file = os.path.join(repo_name, 'datasources', project, ds_to_pub[2])
+        if verbose >= 2:
+            print "Calling tableau-pub-ds.sh with args " + project + ds_file
+        return_code = subprocess.call([os.path.join(home_dir, "scripts", "tableau-pub-ds.sh"), project, ds_file])
+
+# Define usage function
+# Future enhancement: Site will be the highest level directory within repo - so, not passed in
+def usage():
+    print "Usage: " + sys.argv[0] + " [REPO] [SITE]"
+    sys.exit(1)
+
+##################
+### START HERE ###
+##################
+# Global vars
+home_dir = os.environ['HOME']
+verbose = 1
+# USAGE
+if len(sys.argv) == 3:
+    repo = sys.argv[1]
+    if not os.path.isdir(repo):
+        print "Cannot find repository (" + repo + ")"
+        print "Please provide full path to repository directory."
+        usage()
+    site = sys.argv[2]
+else:
+    usage()
+
+
 # Create an array of DataSources
 # Read OS Env var to determine operation
 ds_to_pub = os.environ["DATASOURCES_TO_PUBLISH"]
@@ -73,11 +106,15 @@ if ds_to_pub == '':
     import sys
     sys.exit(0)
 elif ds_to_pub.upper() == 'ALL':
-    read_artefacts_all(os.environ["TAB_INT_REPO_NAME"])
+    ds_list = read_artefacts_all(repo)
 else:
-    read_artefacts_specific()
+    ds_list = read_artefacts_specific()
 
 # Log in to Tableau Server (with helper script)
-#return_code = subprocess.call("~/scripts/tableau-login.sh")
+return_code = subprocess.call([os.path.join(home_dir, "scripts", "tableau-login.sh"), site])
+if return_code != 0:
+    print "Error calling: " + os.path.join(home_dir, "scripts", "tableau-login.sh") + " " + site
+    sys.exit(return_code)
 
 # Iterate through each array calling helper script to publish each element
+publish_datasources(repo, ds_list)
