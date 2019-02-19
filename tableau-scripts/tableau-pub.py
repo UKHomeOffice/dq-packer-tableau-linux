1a8e2a8a2a3ff16eb26c8d4cd864684a24699c25
Ian Relf <ian.relf@gmail.com>
2019-02-19 15:33:31 +0000

    The script should work 100% - with a following wind.

--- tableau-scripts/tableau-pub.py
+++ tableau-scripts/tableau-pub.py
@@ -31,15 +31,15 @@ from os import listdir
 import subprocess
 
 
-# Define a function to read ALL artefacts from Git REPO
+# Define a function to read ALL DataSources from Git REPO
 # Assume that all artefacts are for the SITE passed in to this python script
 # Future enhancement: Site will be the highest level directory within repo
-def read_artefacts_all(repo_name):
-    # Get all DataSource Projects
+def read_datasources_all(repo_name):
     ds_list = []
 
     if verbose >= 1:
         print ("repo name = <" + repo_name + ">")
+    # Get all Projects
     for proj_ds in listdir(os.path.join(repo_name, 'datasources')):
         if verbose >=1:
             print ("proj = <" + proj_ds + ">")
@@ -55,24 +55,111 @@ def read_artefacts_all(repo_name):
         print ds_list
     return ds_list
 
+# Define a function to read ALL WorkBooks from Git REPO
+# Assume that all artefacts are for the SITE passed in to this python script
+# Future enhancement: Site will be the highest level directory within repo
+def read_workbooks_all(repo_name):
+    wb_list = []
 
-# Define a function to read Specific artefacts from environment variable
-def read_artefacts_specific():
-    pass
+    if verbose >= 1:
+        print ("repo name = <" + repo_name + ">")
+    # Get all Projects
+    for proj_wb in listdir(os.path.join(repo_name, 'workbooks')):
+        if verbose >=1:
+            print ("proj = <" + proj_wb + ">")
+        for wb in listdir(os.path.join(repo_name, 'workbooks', proj_wb)):
+            if verbose >= 3:
+                print ("wb = <" + wb + ">")
+            full_wb = []
+            full_wb.append(site)
+            full_wb.append(proj_wb)
+            full_wb.append(wb)
+            wb_list.append(full_wb)
+    if verbose >= 2:
+        print wb_list
+    return wb_list
+
+# Define a function to read Specific DataSources from environment variable
+def read_datasources_specific(repo_name, ds_list_string):
+    ds_list = []
+
+    if verbose >= 2:
+        print ("repo name = <" + repo_name + ">")
+
+    if verbose >= 2:
+        print "ds_list_string = <" + ds_list_string + ">"
+
+    for full_ds in ds_list_string.split(','):
+        if verbose >= 3:
+            print "full_ds = <" + full_ds + ">"
+        ds_elems = full_ds.split('/')
+        if verbose >= 3:
+            print "ds_elems = <"
+            print ds_elems
+            print ">"
+        ds_list.append(ds_elems)
+
+    if verbose >= 2:
+        print ds_list
+    return ds_list
+
+# Define a function to read Specific WorkBooks from environment variable
+def read_workbooks_specific(repo_name, wb_list_string):
+    wb_list = []
+
+    if verbose >= 1:
+        print ("repo name = <" + repo_name + ">")
+
+    if verbose >= 2:
+        print "wb_list_string = <" + wb_list_string + ">"
+
+    for full_wb in wb_list_string.split(','):
+        if verbose >= 3:
+            print "full_wb = <" + full_wb + ">"
+        wb_elems = full_wb.split('/')
+        if verbose >= 3:
+            print "wb_elems = <"
+            print wb_elems
+            print ">"
+        wb_list.append(wb_elems)
+
+    if verbose >= 2:
+        print wb_list
+    return wb_list
+
+
 
 # Define a function to publish all of the artefacts read previously
 def publish_datasources(repo_name, ds_list_to_pub):
     for ds_to_pub in ds_list_to_pub:
         if verbose >= 2:
             print "ds_to_pub = <"
-            for elem in ds_to_pub:
-                print elem
+            print ds_to_pub
             print ">"
         project = ds_to_pub[1]
         ds_file = os.path.join(repo_name, 'datasources', project, ds_to_pub[2])
         if verbose >= 2:
             print "Calling tableau-pub-ds.sh with args " + project + ds_file
         return_code = subprocess.call([os.path.join(home_dir, "scripts", "tableau-pub-ds.sh"), project, ds_file])
+        if return_code != 0:
+            print "Error calling: " + os.path.join(home_dir, "scripts", "tableau-pub-ds.sh") + " " + project + " " + ds_file
+            sys.exit(return_code)
+
+# Define a function to publish all of the artefacts read previously
+def publish_workbooks(repo_name, wb_list_to_pub):
+    for wb_to_pub in wb_list_to_pub:
+        if verbose >= 2:
+            print "wb_to_pub = <"
+            print wb_to_pub
+            print ">"
+        project = wb_to_pub[1]
+        wb_file = os.path.join(repo_name, 'workbooks', project, wb_to_pub[2])
+        if verbose >= 2:
+            print "Calling tableau-pub-wb.sh with args " + project + wb_file
+        return_code = subprocess.call([os.path.join(home_dir, "scripts", "tableau-pub-wb.sh"), project, wb_file])
+        if return_code != 0:
+            print "Error calling: " + os.path.join(home_dir, "scripts", "tableau-pub-wb.sh") + " " + project + " " + wb_file
+            sys.exit(return_code)
 
 # Define usage function
 # Future enhancement: Site will be the highest level directory within repo - so, not passed in
@@ -101,14 +188,17 @@ else:
 # Create an array of DataSources
 # Read OS Env var to determine operation
 ds_to_pub = os.environ["DATASOURCES_TO_PUBLISH"]
+wb_to_pub = os.environ["WORKBOOKS_TO_PUBLISH"]
 
 if ds_to_pub == '':
     import sys
     sys.exit(0)
 elif ds_to_pub.upper() == 'ALL':
-    ds_list = read_artefacts_all(repo)
+    ds_list = read_datasources_all(repo)
+    wb_list = read_workbooks_all(repo)
 else:
-    ds_list = read_artefacts_specific()
+    ds_list = read_datasources_specific(repo, ds_to_pub)
+    wb_list = read_workbooks_specific(repo, wb_to_pub)
 
 # Log in to Tableau Server (with helper script)
 return_code = subprocess.call([os.path.join(home_dir, "scripts", "tableau-login.sh"), site])
@@ -116,5 +206,10 @@ if return_code != 0:
     print "Error calling: " + os.path.join(home_dir, "scripts", "tableau-login.sh") + " " + site
     sys.exit(return_code)
 
-# Iterate through each array calling helper script to publish each element
-publish_datasources(repo, ds_list)
+# Call function to publish each DataSource
+if len(ds_list) > 0:
+    publish_datasources(repo, ds_list)
+
+# Call function to publish each WorkBook
+if len(wb_list) > 0:
+    publish_workbooks(repo, wb_list)
