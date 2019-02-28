eff7fad774218f8b2bed6272ce0f4b88b5cfdc2f
Ian Relf <ian.relf@gmail.com>
2019-02-28 12:37:52 +0000

    Update tableau scripts: 4th Product Key works, remove unused code.

--- tableau-scripts/tableau-license-deactivate.sh
+++ tableau-scripts/tableau-license-deactivate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_1" -u "$TAB_SRV_USER" -p "$TAB_SRV_PASSWORD"
-tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_2" -u "$TAB_SRV_USER" -p "$TAB_SRV_PASSWORD"
-tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_3" -u "$TAB_SRV_USER" -p "$TAB_SRV_PASSWORD"
+tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_1" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_2" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_3" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_4" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+tsm restart --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
--- tableau-scripts/tableau-pub-ds.sh
+++ tableau-scripts/tableau-pub-ds.sh
@@ -14,4 +14,3 @@ then
 fi
 
 tabcmd publish "$DATASOURCE_FILENAME" --project "$PROJECT" --overwrite --db-username "$TAB_DB_USER" --db-password "$TAB_DB_PASSWORD" --save-db-password
-#tabcmd publish \"$DATASOURCE_FILENAME\" --project \"$PROJECT\" --overwrite --db-username "$TAB_DB_USER" --db-password "$TAB_DB_PASSWORD" --save-db-password
