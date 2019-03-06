0e7290ee8eed57c06ee243e5c85731dc0ba84445
Ian Relf <ian.relf@gmail.com>
2019-03-06 14:31:41 +0000

    Use flag long-names

--- tableau-scripts/tableau-license-deactivate.sh
+++ tableau-scripts/tableau-license-deactivate.sh
@@ -4,5 +4,5 @@ tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_1" --username "$TAB_SRV_
 tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_2" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
 tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_3" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
 tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_4" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
-tsm licenses activate --trial -u $TAB_SRV_USER -p $TAB_SRV_PASSWORD
+tsm licenses activate --trial --username $TAB_SRV_USER --password $TAB_SRV_PASSWORD
 tsm restart --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
