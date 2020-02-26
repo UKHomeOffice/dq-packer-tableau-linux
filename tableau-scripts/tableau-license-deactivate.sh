a40284e4ab6e03b7871d6f28bee30e4fdfed3488
Ian Relf <ian.relf@gmail.com>
2020-02-26 15:01:25 +0000

    Enhance license activation/deactivation to work for both Tab Ext and Tab Int

--- tableau-scripts/tableau-license-deactivate.sh
+++ tableau-scripts/tableau-license-deactivate.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
-tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_1" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
-tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_2" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
-tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_3" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
-tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_4" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+if [ ! -z $TAB_PRODUCT_KEY ] # Tableau External
+then
+  tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY"   --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+else # Tableau Internal
+  tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_1" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+  tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_2" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+  tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_3" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+  tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_4" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+fi
 tsm licenses activate --trial --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
-tsm restart --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+tsm restart                   --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
