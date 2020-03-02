6367666d04f4b15bc266bb48a02476dcc7aae679
Ian Relf <ian.relf@gmail.com>
2020-03-02 11:58:36 +0000

    Ask user to confirm deactivation of licenses.

--- tableau-scripts/tableau-license-deactivate.sh
+++ tableau-scripts/tableau-license-deactivate.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+echo "Deactivating licenses..."
+echo "This will removed all of the current licenses from Tableau."
+read -p "Are you sure [y/N]? " -n 1 -r
+echo    # new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Exiting."
+    exit 1
+fi
+
 if [ ! -z $TAB_PRODUCT_KEY ] # Tableau External
 then
   tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY"   --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
