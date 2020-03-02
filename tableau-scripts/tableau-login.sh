070513f272e4248965b9f35b207b2e99d9eea947
Ian Relf <ian.relf@gmail.com>
2020-03-02 21:04:26 +0000

    Updated tableau_pub.py to handle sites.

--- tableau-scripts/tableau-login.sh
+++ tableau-scripts/tableau-login.sh
@@ -7,4 +7,6 @@ fi
 
 SITE=$1
 
-tabcmd login -s localhost --username "$TAB_ADMIN_USER" --password "$TAB_ADMIN_PASSWORD" --site "$SITE"
+err = tabcmd login -s localhost --username "$TAB_ADMIN_USER" --password "$TAB_ADMIN_PASSWORD" --site "$SITE"
+
+return err
