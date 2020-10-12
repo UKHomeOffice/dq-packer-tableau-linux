7b2502bae0ff2a50c9416466da848fefc09cb2ef
Ian Relf <ian.relf@gmail.com>
2020-10-12 13:45:54 +0100

    The env var names are now differnt on the Tableau Server

--- tableau-scripts/tableau-enable-repo-access.sh
+++ tableau-scripts/tableau-enable-repo-access.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tsm data-access repository-access enable --repository-username "$TAB_INT_TABSVR_REPO_USER" --repository-password "$TAB_INT_TABSVR_REPO_PASSWORD" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+tsm data-access repository-access enable --repository-username "$TAB_TABSVR_REPO_USER" --repository-password "$TAB_TABSVR_REPO_PASSWORD" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
