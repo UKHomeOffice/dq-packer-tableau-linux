0129e04760e0e8d3e66d8af558702bb305a3dc0e
Ian Relf <ian.relf@gmail.com>
2021-01-07 14:05:50 +0000

    Now only 3 Product Keys, not 4.

--- tableau-scripts/tableau-license-activate.sh
+++ tableau-scripts/tableau-license-activate.sh
@@ -6,6 +6,5 @@ else # Tableau Internal
   tsm licenses activate --license-key "$TAB_PRODUCT_KEY_1" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
   tsm licenses activate --license-key "$TAB_PRODUCT_KEY_2" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
   tsm licenses activate --license-key "$TAB_PRODUCT_KEY_3" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
-  tsm licenses activate --license-key "$TAB_PRODUCT_KEY_4" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
 fi
 tsm restart --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
--- tableau-scripts/tableau-license-deactivate.sh
+++ tableau-scripts/tableau-license-deactivate.sh
@@ -17,7 +17,6 @@ else # Tableau Internal
   tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_1" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
   tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_2" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
   tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_3" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
-  tsm licenses deactivate --license-key "$TAB_PRODUCT_KEY_4" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
 fi
 tsm licenses activate --trial --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
 tsm restart                   --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
