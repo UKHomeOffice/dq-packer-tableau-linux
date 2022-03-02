f97ae7b83c4df0e9b0234e352eab8cb2c02a0697
IanRelf <ian.relf@digital.homeoffice.gov.uk>
2022-03-02 15:14:20 +0000

    Add error checking.

--- tableau-scripts/tableau-license-activate.sh
+++ tableau-scripts/tableau-license-activate.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+err=0
 if [ ! -z $TAB_PRODUCT_KEY ] # Tableau External
 then
   tsm licenses activate --license-key "$TAB_PRODUCT_KEY"   --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+  err=$((err + $?))
 else # Tableau Internal
   tsm licenses activate --license-key "$TAB_PRODUCT_KEY_1" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+  err=$((err + $?))
   tsm licenses activate --license-key "$TAB_PRODUCT_KEY_2" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+  err=$((err + $?))
   tsm licenses activate --license-key "$TAB_PRODUCT_KEY_3" --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+  err=$((err + $?))
 fi
-tsm restart --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+if (( $err > 0 ))
+then
+  echo "At least one license activation failed - NOT restarting TSM"
+else
+  tsm restart --username "$TAB_SRV_USER" --password "$TAB_SRV_PASSWORD"
+fi
