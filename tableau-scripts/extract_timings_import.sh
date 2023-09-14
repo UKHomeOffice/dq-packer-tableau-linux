2955885b705f72ef964ce2e86e71e0b9d6512db6
Peter Miller <pmiller@Aker-FVFH40E8Q05N.local>
2023-09-14 13:33:46 +0100

    finalised scripts

--- tableau-scripts/extract_timings_import.sh
+++ tableau-scripts/extract_timings_import.sh
@@ -3,4 +3,4 @@
 export PGPASSWORD=$RDS_POSTGRES_SERVICE_PASSWORD
 
 #log into RDS and run the SQL file
-psql postgresql://$RDS_POSTGRES_SERVICE_USER:$RDS_POSTGRES_SERVICE_PASSWORD@$RDS_POSTGRES_ENDPOINT/internal_tableau < /home/tableau_srv/scripts/import_repo.sql
+psql -v tableau_repo_environment=$TABLEAU_REPO_ENVIRONMENT postgresql://$RDS_POSTGRES_SERVICE_USER:$RDS_POSTGRES_SERVICE_PASSWORD@$RDS_POSTGRES_ENDPOINT/internal_tableau < /home/tableau_srv/scripts/import_repo.sql
