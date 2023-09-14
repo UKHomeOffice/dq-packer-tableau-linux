cb7507e4cd521c832d186d70959955bed1faea93
Peter Miller <pmiller@Aker-FVFH40E8Q05N.local>
2023-09-14 15:28:03 +0100

    Improved script syntax

--- tableau-scripts/extract_timings_import.sh
+++ tableau-scripts/extract_timings_import.sh
@@ -2,5 +2,8 @@
 
 export PGPASSWORD=$RDS_POSTGRES_SERVICE_PASSWORD
 
+#PSQL requires single quotes so as to avoid interpreting the environment variable as a column
+export TAB_REPO_ENV="'$TABLEAU_REPO_ENVIRONMENT'"
+
 #log into RDS and run the SQL file
-psql -v tableau_repo_environment=$TABLEAU_REPO_ENVIRONMENT postgresql://$RDS_POSTGRES_SERVICE_USER:$RDS_POSTGRES_SERVICE_PASSWORD@$RDS_POSTGRES_ENDPOINT/internal_tableau < /home/tableau_srv/scripts/import_repo.sql
+psql -v tableau_repo_environment=$TAB_REPO_ENV postgresql://$RDS_POSTGRES_SERVICE_USER:$RDS_POSTGRES_SERVICE_PASSWORD@$RDS_POSTGRES_ENDPOINT/internal_tableau < /home/tableau_srv/scripts/import_repo.sql
